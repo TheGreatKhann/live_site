5ab2569f550b621899ec9bd046be8cadcebf34c9
Walter <walter.kruger@hotmail.com>
2023-05-13 12:27:24 +1000

    Added external nav JSON

--- global.js
+++ global.js
@@ -1,3 +1,14 @@
-function alert_hi() {
-    alert("Hi");
+var nav_json;              // Decleared outside -> global
+async function get_nav() {
+    const response = await fetch('https://thegreatkhann.github.io/live_site/EXTERNAL/nav.JSON');
+    nav_json = await response.json();
+    
+    place_nav();
+}
+
+function place_nav() {
+    const nav = document.getElementsByTagName("nav")[0];
+
+    nav.innerHTML = nav_json.link;
+    //nav.innerHTML = "This is a JavaScript placed nav";
 }
